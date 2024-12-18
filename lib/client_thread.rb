8486bf526816e4bf59d8d4c05244aa93f8fd5688
Vitalijus Desukas <vitalij.desuk@gmail.com>
2024-12-18 23:05:53 +0200

    update compose file

--- lib/client_thread.rb
+++ lib/client_thread.rb
@@ -1,4 +1,5 @@
 # HOW IT WORKS?
+
 # Teltonika module communicating with the server. First time module is authenticated by IMEI,
 # second time data from module decoded and confirmation about number_of_rec is sent back to the module.
 # Positive response is sent, if decoded num_of_rec matching with what
@@ -7,10 +8,18 @@
 # Module is sending data packets every 2min.
 # When num_of_rec matching Tracker data, like latitude, longitude, speed is being saved to the DB.
 
+# After you have run required docker compose commands you might want to test TCP server
+# on your local machine.
+# First, confirm your TCP Server is running:
+# lsof -iTCP -sTCP:LISTEN -n -P
+
+# Second, send message to the TCP Server using shell on local machine:
+# echo -n -e '\x00\x02000F333536333037303432343431303133\xAB\xCD' | nc localhost 65432
+
 require 'socket'
 require 'date'
-require 'data_decoder'
-require 'trackers_data'
+require_relative 'data_decoder'
+require_relative 'trackers_data'
 
 class ClientThread
   def initialize(port)
@@ -76,3 +85,82 @@ end # end of class
 
 new_thread = ClientThread.new(65432)
 p new_thread.run
+
+
+# class ClientThread
+#   NEGATIVE_RESPONSE = [0x00].pack("C")
+#   AUTH_RESPONSE = [0x01].pack("C")
+#
+#   def initialize(port)
+#     @server = TCPServer.open(port)
+#     @imei = "unknown"
+#   end
+#
+#   def log(message)
+#     "#{Time.now.utc.strftime('%FT%T')} #{message}"
+#   end
+#
+#   def handle_client(client)
+#     2.times do |step|
+#       begin
+#         buffer = client.recv(8192)
+#         length, imei = buffer.unpack("Sa*")
+#         data = buffer.unpack("H*").first
+#
+#         case step
+#         when 0
+#           handle_authentication(client, imei)
+#         when 1
+#           handle_data_decoding(client, data)
+#         else
+#           client.send(NEGATIVE_RESPONSE, 0)
+#         end
+#       rescue SocketError
+#         log_and_report("Socket has failed")
+#       end
+#     end
+#   ensure
+#     client.close if client
+#   end
+#
+#   def handle_authentication(client, imei)
+#     @imei = imei
+#     log_and_report("Device Authenticated | IMEI: #{@imei}")
+#     client.send(AUTH_RESPONSE, 0)
+#   end
+#
+#   def handle_data_decoding(client, data)
+#     decoder = DataDecoder.new(data, @imei)
+#     if decoder.nil?
+#       log_and_report("FMT100 data decoding error: #{decoder}")
+#       client.send(NEGATIVE_RESPONSE, 0)
+#       return
+#     end
+#
+#     num_of_rec = decoder.number_of_rec
+#     if num_of_rec == 0
+#       client.send(NEGATIVE_RESPONSE, 0)
+#     else
+#       log_and_report("Decoded data: #{decoder.decode}")
+#       client.send([num_of_rec].pack("L>"), 0)
+#       log_and_report("Done! Closing Connection")
+#     end
+#   end
+#
+#   def log_and_report(message)
+#     puts log(message)
+#     # Rollbar.log("error", message) if Rollbar is enabled
+#   end
+#
+#   def run
+#     log_and_report("Started TCP Server")
+#     loop do
+#       Thread.start(@server.accept) do |client|
+#         handle_client(client)
+#       end
+#     end
+#   end
+# end
+#
+# new_thread = ClientThread.new(65432)
+# new_thread.run
